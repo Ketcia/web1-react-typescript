fc891b625b3fc044665350b9c31e25eb1edfea04
Ketcia <leticiarafael98@gmail.com>
2022-02-01 16:39:25 -0300

    01/02

---
+++ src/routes/PrivateRoutes.tsx
@@ -0,0 +1,10 @@
+import { useAuth } from "hooks/auth";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  Redirect,
+  Route,
+  RouteComponentProps,
+  RouteProps,
+} from "react-router-dom";
+
+
