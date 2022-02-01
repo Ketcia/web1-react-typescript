74760c2dc4123c4fb2fbde07a7c36a2b03f05bfc
Ketcia <leticiarafael98@gmail.com>
2022-02-01 16:51:41 -0300

    Private Routes

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -7,4 +7,40 @@ import {
   RouteProps,
 } from "react-router-dom";
 
+interface PrivateRouteParams extends RouteProps {
+    component:
+      | React.ComponentType<RouteComponentProps<any>>
+      | React.ComponentType<any>;
+  }
+
+  export function PrivateRoute({
+    component: Component,
+    ...rest
+  }: PrivateRouteParams) {
+    const { token, loadUserStorageData} = useAuth();
+  
+    const fetchData = useCallback(async () => {
+      await loadUserStorageData();
+    }, [loadUserStorageData]);
+  
+    useEffect(() => {
+      fetchData();
+    }, [fetchData]);
+  
+    return (
+          <Route
+            {...rest}
+            render={(props) =>
+              token ? (
+                <Component {...props} />
+              ) : (
+                <Redirect
+                  to={{ pathname: "/login", state: { from: props.location } }}
+                />
+              )
+            }
+          />
+  
+    );
+  }
 
